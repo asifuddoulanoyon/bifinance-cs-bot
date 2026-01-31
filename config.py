efcdf691382ce3038ff540694bd6d5d520ab6bf0
asifuddoulanoyon <105268068+asifuddoulanoyon@users.noreply.github.com>
2026-02-01 03:37:19 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-BOT_OWNER_ID = 1675295056
 
-# List of agent user IDs, can update via /addagent /removeagent
-AGENTS = []
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is missing")
+
+# PUT YOUR REAL TELEGRAM ID HERE
+ADMIN_IDS = [1675295056]
+AGENT_IDS = [1675295056]  # you can be agent + admin
